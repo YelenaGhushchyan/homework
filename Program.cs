5527f398cff6781ec2a23afbe57f1742943c5aa7
YelenaGhushchyan <yelghushchyan12@mail.ru>
2025-02-15 12:00:09 +0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,25 +1,27 @@
-﻿namespace ConsoleApp9;
-// Task 9: Finding the Longest Word in a Sentence
-// Task: Ask the user for a sentence and use a loop to determine the longest word.
+﻿using System;
+//Task 1: Fibonacci Sequence Generator
+// Task: Write a program that generates the first N numbers of the Fibonacci sequence using a loop.
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Write sentence ");
-        string? sentence = Console.ReadLine();
-        string[] arr = sentence.Split(" ");
-        int maxCount = 0;
-        string maxWord = "";
-        for (int i = 0; i < arr.Length; i++)
+        Console.WriteLine("enter your number");
+        int n = int.Parse(Console.ReadLine());
+        int first = 0;
+        int second = 1;
+        if (n > 1)
         {
-            if (arr[i].Length > maxCount)
-            {
-                maxCount = arr[i].Length;
-                maxWord = arr[i];
-            }
+            Console.Write(second);
         }
-        Console.WriteLine("{0} {1}",maxWord,maxCount);
-        
+
+        for (int i = 2; i <= n; i++)
+        {
+            int next = first + second;
+            Console.Write(next);
+            first = second;
+            second = next;
+        }
+        Console.WriteLine();
     }
 }
