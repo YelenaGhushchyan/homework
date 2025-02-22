7b507893ab99afe4dbbf912d4ec17f5e20fa38b6
YelenaGhushchyan <yelghushchyan12@mail.ru>
2025-02-22 23:09:51 +0400

    Update program.cs

--- Smartwatch Step Counter/program.cs
+++ Smartwatch Step Counter/program.cs
@@ -2,23 +2,29 @@ public class Smartwatch
 {
     public string ownerName;
     public int stepCount;
+    public Smartwatch(string name){
+        ownerName = name;
+        stepCount = 0;
+    }
 
     public void AddSteps(int steps)
     {
         stepCount += steps;
     }
 
-    public void ShowSteps()
+   public void ShowSteps()
     {
-        Console.WriteLine("Steps: " + stepCount);
+        
+        Console.WriteLine($"{ownerName}'s stepcount is  {stepCount} ");
     }
+}
 }
 
 class Program
 {
     static void Main(string[] args)
     {
-        Smartwatch smartwatch = new Smartwatch();
+        Smartwatch smartwatch = new Smartwatch("Ann");
         smartwatch.AddSteps(5);
         smartwatch.AddSteps(12);
         smartwatch.ShowSteps();
